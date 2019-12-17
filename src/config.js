1d24392793573cb9a0b242744c7a46ebff5245bc
Pankaj <pspankajsingh395@gmail.com>
2019-12-17 20:40:01 +0530

    push changes

--- src/config.js
+++ src/config.js
@@ -7,12 +7,12 @@ if(process.env.NODE_ENV==="development"){
         // config.getUsers = "http://localhost:5000/api/users"
         // config.update = "http://localhost:5000/api/update"
         // config.sendMail = "http://localhost:5000/api/send-mail"
-        config.login = "https://quiet-bayou-33585.herokuapp.com/api/login"
-        config.signup = "https://quiet-bayou-33585.herokuapp.com/api/register"
-        config.forgotPass = "https://quiet-bayou-33585.herokuapp.com/api/forgot-pass"
-        config.getUsers = "https://quiet-bayou-33585.herokuapp.com/api/users"
-        config.update = "https://quiet-bayou-33585.herokuapp.com/api/update"
-        config.sendMail = "https://quiet-bayou-33585.herokuapp.com/api/send-mail"
+        config.login = "http://quiet-bayou-33585.herokuapp.com/api/login"
+        config.signup = "http://quiet-bayou-33585.herokuapp.com/api/register"
+        config.forgotPass = "http://quiet-bayou-33585.herokuapp.com/api/forgot-pass"
+        config.getUsers = "http://quiet-bayou-33585.herokuapp.com/api/users"
+        config.update = "http://quiet-bayou-33585.herokuapp.com/api/update"
+        config.sendMail = "http://quiet-bayou-33585.herokuapp.com/api/send-mail"
     
     }
 else
